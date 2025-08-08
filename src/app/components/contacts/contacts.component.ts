43393c92cc8e5832c484f25ec0902e38411b52bb
Ranim Bms <ranimboumessouer@gmail.com>
2025-08-08 18:49:11 +0100

    registerup

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -2,20 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { PersonalComponent } from './personal/personal.component';
 import { ContactsFilterComponent } from './contacts-filter/contacts-filter.component';
 import { BreadcrumbComponent } from '../../shared/components/breadcrumb/breadcrumb.component';
+import { FeatherIconComponent } from '../../shared/components/feather-icon/feather-icon.component';
 
 @Component({
     selector: 'app-contacts',
     templateUrl: './contacts.component.html',
     styleUrls: ['./contacts.component.scss'],
-    imports: [BreadcrumbComponent, ContactsFilterComponent, PersonalComponent]
+    imports: [BreadcrumbComponent,FeatherIconComponent, ContactsFilterComponent, PersonalComponent]
 })
 
 export class ContactsComponent implements OnInit {
+
+  public show: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
- 
+  showPassword() {
+    this.show = !this.show;
+  }
 
 }
