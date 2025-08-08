5b26f02b436fdce01ab6610c00fefdbf9fd209d6
Khalil <118051670+khalilsed@users.noreply.github.com>
2025-08-08 22:37:46 +0100

    Update patients.service.ts

--- src/app/services/patients.service.ts
+++ src/app/services/patients.service.ts
@@ -1,6 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+export interface Patient {
+  id?: number;
+  dateNaissance: string;
+  user: {
+    id: number;
+    nom: string;
+    prenom: string;
+    email: string;
+    tlf: string;
+    adresse: string;
+    nomComplet: string;
+  };
+  rendezVous?: any[];
+}
+
+export interface CreatePatientRequest {
+  dateNaissance: string;
+  user: number; // ID de l'utilisateur
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +32,34 @@ export class PatientsService {
 
   getAllPatient(): Observable<any> {
     return this.http.get<any>(this.patientsUrl);
+  }
+
+  /**
+   * Récupérer un patient par ID
+   */
+  getPatientById(id: number): Observable<Patient> {
+    return this.http.get<Patient>(`${this.apiUrl}/${id}`);
+  }
+
+  /**
+   * Créer un nouveau patient
+   */
+  createPatient(patientData: CreatePatientRequest): Observable<Patient> {
+    return this.http.post<Patient>(this.apiUrl, patientData, this.httpOptions);
+  }
+
+  /**
+   * Mettre à jour un patient
+   
+  updatePatient(id: number, patientData: Partial<CreatePatientRequest>): Observable<Patient> {
+    return this.http.put<Patient>(`${this.apiUrl}/${id}`, patientData, this.httpOptions);
+  }*/
+
+  /**
+   * Supprimer un patient
+   */
+  deletePatient(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
 
